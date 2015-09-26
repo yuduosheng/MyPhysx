61274fd8558020b288f7589bbe00460cea676217
yuduosheng <yuduosheng@hotmail.com>
2015-09-26 19:49:37 +0900

    window + dx init

--- MyProgram/MyDirectX12/DirectX12Test/main.cpp
+++ MyProgram/MyDirectX12/DirectX12Test/main.cpp
@@ -1,13 +1,28 @@
+#include <windows.h>
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
-
+#include <wrl.h>
+#include "d3dx12.h"
 #include <string>
 #include <fstream>
-//#include <windows.h>
+
 
 using namespace std;
+using namespace Microsoft::WRL;
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw;
+	}
+}
+
+
+
+
 // Viewport dimensions.
 UINT windowWidth = 800;
 UINT windowHeight = 800;
@@ -15,7 +30,24 @@ float m_aspectRatio;
 wstring windowName = L"First Window.";
 
 
+// Pipeline objects.
+const UINT FrameCount = 2;
+ComPtr<IDXGISwapChain3> m_swapChain;
+ComPtr<ID3D12Device> m_device;
+ComPtr<ID3D12Resource> m_renderTargets[FrameCount];
+ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+ComPtr<ID3D12CommandQueue> m_commandQueue;
+ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+ComPtr<ID3D12PipelineState> m_pipelineState;
+ComPtr<ID3D12GraphicsCommandList> m_commandList;
+UINT m_rtvDescriptorSize;
 
+
+// Synchronization objects.
+UINT m_frameIndex;
+HANDLE m_fenceEvent;
+ComPtr<ID3D12Fence> m_fence;
+UINT64 m_fenceValue;
 
 void OnInit();
 void OnUpdate();
@@ -28,9 +60,11 @@ void GetHardwareAdapter(_In_ IDXGIFactory4* pFactory, _Outptr_result_maybenull_ 
 
 static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
+void LoadPipeline();
+
 int Run(HINSTANCE hInstance, int nCmdShow);
-
-
+void PopulateCommandList();
+void WaitForPreviousFrame();
 // Window handle.
 HWND m_hwnd;
 
@@ -143,20 +177,197 @@ int Run(HINSTANCE hInstance, int nCmdShow)
 	// Return this part of the WM_QUIT message to Windows.
 	return static_cast<char>(msg.wParam);
 }
+// Load the rendering pipeline dependencies.
+void LoadPipeline()
+{
+#ifdef _DEBUG
+	// Enable the D3D12 debug layer.
+{
+	ComPtr<ID3D12Debug> debugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+	{
+		debugController->EnableDebugLayer();
+	}
+}
+#endif
+
+ComPtr<IDXGIFactory4> factory;
+if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&factory))))
+{
+	throw;
+}
+ComPtr<IDXGIAdapter1> hardwareAdapter;
+GetHardwareAdapter(factory.Get(), &hardwareAdapter);
+
+if (FAILED(D3D12CreateDevice(hardwareAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device))))
+throw;
+
+// Describe and create the command queue.
+D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+if (FAILED(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue))))
+throw;
+
+// Describe and create the swap chain.
+DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+swapChainDesc.BufferCount = FrameCount;
+swapChainDesc.BufferDesc.Width = windowWidth;
+swapChainDesc.BufferDesc.Height = windowHeight;
+swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+swapChainDesc.OutputWindow = m_hwnd;
+swapChainDesc.SampleDesc.Count = 1;
+swapChainDesc.Windowed = TRUE;
+
+ComPtr<IDXGISwapChain> swapChain;
+if (FAILED(factory->CreateSwapChain(
+	m_commandQueue.Get(),		// Swap chain needs the queue so that it can force a flush on it.
+	&swapChainDesc,
+	&swapChain
+	)))throw;
+
+if (FAILED(swapChain.As(&m_swapChain)))throw;
+
+// This sample does not support fullscreen transitions.
+if (FAILED(factory->MakeWindowAssociation(m_hwnd, DXGI_MWA_NO_ALT_ENTER)))throw;
+
+m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+// Create descriptor heaps.
+{
+	// Describe and create a render target view (RTV) descriptor heap.
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+	rtvHeapDesc.NumDescriptors = FrameCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	if (FAILED(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap))))throw;
+
+	m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+}
+
+// Create frame resources.
+{
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+	// Create a RTV for each frame.
+	for (UINT n = 0; n < FrameCount; n++)
+	{
+		if (FAILED(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n]))))throw;
+		m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
+		rtvHandle.Offset(1, m_rtvDescriptorSize);
+	}
+}
+
+if (FAILED(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator))))throw;
+}
+
+
+
+// Load the sample assets.
+void LoadAssets()
+{
+	// Create the command list.
+	ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)));
+
+	// Command lists are created in the recording state, but there is nothing
+	// to record yet. The main loop expects it to be closed, so close it now.
+	ThrowIfFailed(m_commandList->Close());
+
+	// Create synchronization objects.
+	{
+		ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+		m_fenceValue = 1;
+
+		// Create an event handle to use for frame synchronization.
+		m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+		if (m_fenceEvent == nullptr)
+		{
+			ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()));
+		}
+	}
+}
+void PopulateCommandList()
+{
+	// Command list allocators can only be reset when the associated 
+	// command lists have finished execution on the GPU; apps should use 
+	// fences to determine GPU execution progress.
+	ThrowIfFailed(m_commandAllocator->Reset());
+
+	// However, when ExecuteCommandList() is called on a particular command 
+	// list, that command list can then be reset at any time and must be before 
+	// re-recording.
+	ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()));
+
+	// Indicate that the back buffer will be used as a render target.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+
+	// Record commands.
+	const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+	// Indicate that the back buffer will now be used to present.
+	m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex].Get(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	ThrowIfFailed(m_commandList->Close());
+}
+
+void WaitForPreviousFrame()
+{
+	// WAITING FOR THE FRAME TO COMPLETE BEFORE CONTINUING IS NOT BEST PRACTICE.
+	// This is code implemented as such for simplicity. More advanced samples 
+	// illustrate how to use fences for efficient resource usage.
+
+	// Signal and increment the fence value.
+	const UINT64 fence = m_fenceValue;
+	ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fence));
+	m_fenceValue++;
+
+	// Wait until the previous frame is finished.
+	if (m_fence->GetCompletedValue() < fence)
+	{
+		ThrowIfFailed(m_fence->SetEventOnCompletion(fence, m_fenceEvent));
+		WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+
+	m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+}
+
 
 void OnInit()
 {
+	LoadPipeline();
+	LoadAssets();
 }
 void OnUpdate()
 {
 }
 void OnRender()
 {
+	// Record all the commands we need to render the scene into the command list.
+	PopulateCommandList();
+
+	// Execute the command list.
+	ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+	m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+	// Present the frame.
+	ThrowIfFailed(m_swapChain->Present(1, 0));
+
+	WaitForPreviousFrame();
 }
 void OnDestroy()
 {
+	// Wait for the GPU to be done with all resources.
+	WaitForPreviousFrame();
+
+	CloseHandle(m_fenceEvent);
 }
 bool OnEvent(MSG msg)
 {
-	return true;
+	return false;
 }
